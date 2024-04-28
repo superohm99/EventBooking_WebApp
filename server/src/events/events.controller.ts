8429cb1622dda39fd6f189cafbe7dfebe70fce16
Thanasak Songsri <Thanasuksongsriohm@gmail.com>
2024-04-28 22:58:13 +0700

    backend 6 thanasak

--- server/src/events/events.controller.ts
+++ server/src/events/events.controller.ts
@@ -1,16 +1,29 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { EventsService } from './events.service';
-import { CreateEventDto } from './dto/create-event.dto';
+import { CreateEventDto, CreateEventSchDto, CreateVenueDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { Public } from 'src/users/common/decorators/src/common/decorators';
 
+@Public()
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
-  @Post()
+  @Post('create_event')
   create(@Body() createEventDto: CreateEventDto) {
-    return this.eventsService.create(createEventDto);
+    return this.eventsService.create_event(createEventDto);
   }
+
+  @Post('create_eventsch')
+  create_sch(@Body() eventschDto: CreateEventSchDto) {
+    return this.eventsService.create_eventsch(eventschDto);
+  }
+
+  @Post('create_venue')
+  create_venue(@Body() venueDto: CreateVenueDto) {
+    return this.eventsService.create_venue(venueDto);
+  }
+
 
   @Get()
   findAll() {
