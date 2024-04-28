c0d32bc715f5f9dbe53ea38bb0950adf194b7328
Thanasak Songsri <Thanasuksongsriohm@gmail.com>
2024-04-28 17:09:12 +0700

    backend update ohm

--- server/src/event_schedule/dto/update-event_schedule.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEventScheduleDto } from './create-event_schedule.dto';
-
-export class UpdateEventScheduleDto extends PartialType(CreateEventScheduleDto) {}
--- server/src/user_info/dto/update-user_info.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserInfoDto } from './create-user_info.dto';
-
-export class UpdateUserInfoDto extends PartialType(CreateUserInfoDto) {}
