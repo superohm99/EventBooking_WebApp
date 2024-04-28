1fba9d73da1a82e5d47f9c99519078c3e22bd717
Teeruth Ieowsakulrat <teeruth2546@gmail.com>
2024-04-28 16:32:47 +0700

    Before pull login

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConcertModule } from './concert/concert.module';
 import { UsersModule } from './users/users.module';
 import { AtStrategy, RtStrategy } from './users/strategies';
 import { APP_GUARD } from '@nestjs/core';
@@ -17,7 +16,7 @@ import { ReserveModule } from './reserve/reserve.module';
 import { EventScheduleModule } from './event_schedule/event_schedule.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://teeruth2546:7mawijIAr3yW510N@ruth.jtq6v9f.mongodb.net/'),ConcertModule, UsersModule, EventsModule, HistoryModule, VenueModule, SeatsModule, TicketModule, UserInfoModule, ReserveModule, EventScheduleModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://teeruth2546:7mawijIAr3yW510N@ruth.jtq6v9f.mongodb.net/'), UsersModule, EventsModule, HistoryModule, VenueModule, SeatsModule, TicketModule, UserInfoModule, ReserveModule, EventScheduleModule],
   controllers: [AppController],
   providers: [AppService,{
     provide: APP_GUARD,
