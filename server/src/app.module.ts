68f19ba6574e3da1ddfcd87b22af0aa6cf73f2a3
Thanasak Songsri <Thanasuksongsriohm@gmail.com>
2024-04-28 17:13:16 +0700

    backend thanasak 2

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,12 +7,11 @@ import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './users/common/guards';
 import { EventsModule } from './events/events.module';
 import { HistoryModule } from './history/history.module';
-import { VenueModule } from './venue/venue.module';
 import { ReserveModule } from './reserve/reserve.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://thanasuksongsriohm:IV3OE0pdQVwKVSbB@myticketdb.vs5a23w.mongodb.net/'), UsersModule, EventsModule, HistoryModule, VenueModule, ReserveModule,],
+  imports: [MongooseModule.forRoot('mongodb+srv://thanasuksongsriohm:IV3OE0pdQVwKVSbB@myticketdb.vs5a23w.mongodb.net/'), UsersModule, EventsModule, HistoryModule, ReserveModule,],
   controllers: [AppController],
   providers: [AppService,{
     provide: APP_GUARD,
--- server/src/venue/dto/update-venue.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateVenueDto } from './create-venue.dto';
-
-export class UpdateVenueDto extends PartialType(CreateVenueDto) {}
