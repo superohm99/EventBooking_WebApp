4d3a6fb5accb82a6147f5c998cf5add4d945c5c3
Prataad <pondaeroz008y@gmail.com>
2024-05-01 15:48:05 +0700

    update get all history to load into frontend history page

--- server/src/history/dto/create-history.dto.ts
+++ server/src/history/dto/create-history.dto.ts
@@ -1,1 +1,5 @@
-export class CreateHistoryDto {}
+export class CreateHistoryDto {
+    private readonly payment_status: boolean;
+    private readonly reserve_timestamp: Date;
+    private readonly reserve: string;
+}
