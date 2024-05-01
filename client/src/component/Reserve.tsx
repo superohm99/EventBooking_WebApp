ed33fae904d4938c12ca67284f381a39dff6b7de
Thanasak Songsri <Thanasuksongsriohm@gmail.com>
2024-05-01 23:30:04 +0700

    FB Reserve Filter Poster

--- client/src/component/Reserve.tsx
+++ client/src/component/Reserve.tsx
@@ -4,18 +4,29 @@ import Leftbar from './Leftbar'
 import Rightbar from './Rightbar'
 import Bottombar from './Bottombar'
 import Reserve_container from './Reserve_container'
+import { useState } from 'react'
+
 
 
 function Reserve() {
+  
+  const [filter, setFilter] = useState('');
+  
+  const handleButton = (data: string) => {
+    console.log("fuck");
+    console.log(data)
+    setFilter(data);
+  };
 
+  
   return (
     <div>
       <Navbar/>
       <div className='sidebar'>
-      <Leftbar/>
+      <Leftbar result={handleButton}/>
       
         <div className='colunm-container'>
-          <Reserve_container/>
+          <Reserve_container filter={filter}/>
           <div>
               <br/>
               <br/>
