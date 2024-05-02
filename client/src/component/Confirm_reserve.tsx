89fb252ba1e2344f3702ef6b93939ae736863560
Thanasak Songsri <Thanasuksongsriohm@gmail.com>
2024-05-02 22:22:40 +0700

    SET CONTROLLER BACKEND Reserve

--- client/src/component/Confirm_reserve.tsx
+++ client/src/component/Confirm_reserve.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
 import Bottombar from './Bottombar'
 import Form_reserve from './Form_reserve'
 import '../style/Form_reserve.css'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 
 function Confirm_reserve() {
+
+  const params = useParams()
+  console.log(params.Id)
+
+  useEffect(()=>{
+    axios.get(`http://localhost:3001/reserve/${params.Id}`)
+  },[])
+
   return (
     <div>
         <Navbar/>
@@ -15,6 +25,7 @@ function Confirm_reserve() {
         <div className='Tone-2'>
             <div>
                 <h3>RESERVE TICKET</h3>
+                {JSON.stringify(params)}
             </div>
 
             <input placeholder='FirstName'></input>
